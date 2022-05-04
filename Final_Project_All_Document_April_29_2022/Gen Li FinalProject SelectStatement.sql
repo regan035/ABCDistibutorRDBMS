c6b18c38e4b75791a44e585073f3a05fb6da74e1
regan035 <regan035@gmail.comgit config --global user.name regan035git statusgit config --global user.email regan035@gmail.com>
2022-05-04 00:54:45 -0500

    update tables and inputs

--- Final_Project_All_Document_April_29_2022/Gen Li FinalProject SelectStatement.sql
+++ Final_Project_All_Document_April_29_2022/Gen Li FinalProject SelectStatement.sql
@@ -1,1 +1,6 @@
- SELECT * FROM ABCDistributorDB.state;
+SELECT * FROM ABCDistributorDB.address;
+SELECT * FROM ABCDistributorDB.contact;
+SELECT * FROM ABCDistributorDB.customer;
+SELECT * FROM ABCDistributorDB.vendor;
+SELECT * FROM ABCDistributorDB.product;
+SELECT * FROM ABCDistributorDB.vendor_product;
