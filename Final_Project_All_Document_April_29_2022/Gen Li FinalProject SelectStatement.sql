1a1348d6f91523eb9e44fd70a293008ce9e4bd6b
regan035 <regan035@gmail.comgit config --global user.name regan035git statusgit config --global user.email regan035@gmail.com>
2022-05-05 11:41:18 -0500

    update select statement

--- Final_Project_All_Document_April_29_2022/Gen Li FinalProject SelectStatement.sql
+++ Final_Project_All_Document_April_29_2022/Gen Li FinalProject SelectStatement.sql
@@ -26,11 +26,12 @@ country.country_id = 3
 ;
 
 
--- Display all Non-Autoreorder inventories with product information
+-- 2.Display all Non-Autoreorder inventories with product information
 SELECT 
 product.product_sku, 
 product.product_name,
 product.product_discription,
+product_category.product_category_name,
 vendor_product.vendor_bulk_price,
 unit.unit_name,
 inventory.inv_storage_location,
@@ -44,11 +45,13 @@ INNER JOIN unit
 ON unit.unit_id = vendor_product.vendor_bulk_unit_id
 INNER JOIN inventory
 ON inventory.inv_product_id = vendor_product.vendor_product_id
+INNER JOIN product_category
+ON product_category.product_category_id = product.product_category_id
 INNER JOIN vendor
 ON vendor.vendor_id = vendor_product.vendor_id
 WHERE inventory.inv_auto_reorder = 'NO';
 
--- Display all unpaid invoices
+-- 3.Display all unpaid invoices
 SELECT
 invoice.invoice_num,
 customer.customer_num,
@@ -76,7 +79,7 @@ INNER JOIN account_transaction
 ON account_transaction.account_transaction_id = invoice.invoice_transaction_id
 WHERE account_transaction.account_payment_paid = '';
 
--- List all stores addresses
+-- 4.List all stores locations
 SELECT 
 store_location.store_id,
 address.address_street,
@@ -97,7 +100,7 @@ ON country.country_id = state.state_country_id
 INNER JOIN zip_code
 ON zip_code.zip_code_id = address.address_zip_code_id;
 
--- List all customers located in the Califonioa USA.
+-- 5.List all customers located in the Califonioa USA.
 SELECT
 customer.customer_num,
 contact.first_name,
@@ -124,7 +127,7 @@ INNER JOIN zip_code
 ON zip_code.zip_code_id = address.address_zip_code_id
 WHERE state.state_id = 001;
 
--- List all grocery product stock status
+-- 6.List all grocery product stock status
 SELECT
 product.product_name,
 product.product_sku,
@@ -150,6 +153,33 @@ INNER JOIN product_category
 ON product_category.product_category_id=product.product_category_id
 WHERE product_category.product_category_name='Grocery';
 
-
+-- 7.List the contact persons of all vendors on file order by vendor number
+SELECT
+vendor.vendor_num,
+contact.first_name,
+contact.last_name,
+contact.orginazation_name,
+contact.primary_phone,
+contact.fax,
+contact.email,
+address.address_street,
+city.city_name,
+state.state_name,
+country.country_name,
+zip_code.zip_code_num
+FROM contact
+INNER JOIN vendor
+ON vendor.vendor_contact_id = contact.contact_id
+INNER JOIN address
+ON address.address_id = contact.address_id
+INNER JOIN city
+ON city.city_id = address.address_city_id
+INNER JOIN state
+ON state.state_id = city.city_state_id
+INNER JOIN country
+ON country.country_id = state.state_country_id
+INNER JOIN zip_code
+ON zip_code.zip_code_id = address.address_zip_code_id
+ORDER BY vendor_num
 
 
